2a14566784f95b21496c83640401f11eaf466689
HoangTitannn <ducthao509@gmail.com>
2024-08-23 08:45:00 +0700

    Add google Ads

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,19 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8553990420699602"
+          crossOrigin="anonymous"
+        ></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  );
+  )
 }
--- src/pages/api/[...path].ts
+++ src/pages/api/[...path].ts
@@ -17,18 +17,20 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>,
 ) {
-  req.headers.cookie = ''
-  
-  proxy.web(req, res, { 
-    target: process.env.API_URL,
-    changeOrigin: true,
-    selfHandleResponse: false
-  }, (err) => {
-    if (err) {
-      console.error('Proxy error:', err);
-      res.status(500).json({ error: 'Proxy error' });
-    }
-  });
+  return new Promise((resolve) => {
+    req.headers.cookie = ''
 
-  // res.status(200).json({ name: "Catch all paths of products" });
+    proxy.web(req, res, {
+      target: process.env.API_URL,
+      changeOrigin: true,
+      selfHandleResponse: false
+    }, (err) => {
+      if (err) {
+        console.error('Proxy error:', err);
+        res.status(500).json({ error: 'Proxy error' });
+      }
+    });
+
+    proxy.once('proxyRes', () => {})
+  })
 }
